6dd3aadc300ba4cbf68e1c37703a6fd5169ab2c4
PajaspaceNet <elektrosoft.paja@gmail.com>
2025-08-25 11:16:14 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
 
+from sqlalchemy import Table, Column, Integer, String, Boolean
+from database import metadata
+
+# Tabulka pro úkoly
+tasks = Table(
+    "tasks",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String, nullable=False),
+    Column("description", String),
+    Column("completed", Boolean, default=False)
+)
