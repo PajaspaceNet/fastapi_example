7ccc197acf8023507768e0b9639fe6d6a2de8cb0
PajaspaceNet <elektrosoft.paja@gmail.com>
2025-08-25 11:15:35 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,9 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+database = Database(DATABASE_URL)
+metadata = MetaData()
+engine = create_engine(str(DATABASE_URL).replace("+aiosqlite", ""), echo=True)
 
